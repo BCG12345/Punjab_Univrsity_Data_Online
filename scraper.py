f2316be582526bb261969d46fb2b0b9b9bc35cc0
BCG12345 <30796091+BCG12345@users.noreply.github.com>
2024-01-13 17:57:48 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,39 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+
+
+
+
+
+import requests
+
+url = 'http://pu.edu.pk/home/results_show/2167'
+payload = {
+    'result_title': 'Bachelor of Commerce Part-II, Annual Examination 2008',
+    'roll_no': '2',
+    'submitcontact': 'Submit'
+}
+
+# Add headers as observed in the DevTools
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Referer':url,
+    'Host': 'pu.edu.pk',
+    'Origin': 'http://pu.edu.pk',
+    'Upgrade-Insecure-Requests': '1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+}
+
+response = requests.post(url, data=payload, headers=headers)
+
+if response.status_code == 200:
+    data = response.text
+    print(data)
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
